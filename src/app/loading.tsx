eebebc2e25b509e02669c14cc5eda17a8ffd53ec
Md Sabbir Hossain <hossainsabbir309@gmail.com>
2026-09-26 22:47:20 +0600

    all done

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,6 +2,7 @@ const Loading = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-lg"></span>
+      <p className="ml-3">Loading workouts…</p>
     </div>
   );
 };
